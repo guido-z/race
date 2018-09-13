a8885d4df8aeb332d48cd4a4dbe79e4036cbb16c
guidoz <guidoz@LAPTOP-RMMS2JC1>
2018-09-13 09:39:46 -0300

    add webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/main.ts',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: '/node_modules/'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        publicPath: '/dist/'
+    }
+}
