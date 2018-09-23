78cc54a147a6bbcc6fa6a5afd603eb120eb8d981
Guido <guido.zaglul@gmail.com>
2018-09-22 22:15:46 -0300

    add socket.io client and connection to server

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,6 +1,8 @@
 import { Game } from './game';
+import * as io from "socket.io-client";
 
 window.addEventListener('load', () => {
+    const socket = io('http://localhost:8081');
     const context = document.querySelector('canvas').getContext('2d');
     const game = new Game(context);
     game.run();
