05adc6efcf77c564d629eaecc4e9e0991ed003ce
guidoz <guidoz@LAPTOP-RMMS2JC1>
2018-09-29 13:48:36 -0300

    add sendName and disconnect handlers

--- server/src/main.js
+++ server/src/main.js
@@ -1,10 +1,17 @@
 const io = require('socket.io')();
+const manager = require('./web-socket/web-socket-manager')(io);
 
 const server = io.listen(8081);
 
 io.on('connection', client => {
     io.to(client.conn.id).emit('requestName');
     
-    client.on('sendName', ([message]) => {        
+    client.on('sendName', ([{ payload: { name } }]) => {        
+        manager.addClient(client.conn.id, name);
+        manager.emit('playerJoin', { name });
+    });
+
+    client.on('disconnect', () => {
+        manager.removeClient(client.conn.id);
     });
 });
