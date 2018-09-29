eba1638ccf2546baf3258352525f2973202fab98
guidoz <guidoz@LAPTOP-RMMS2JC1>
2018-09-29 12:35:16 -0300

    refactor game creation

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,11 +1,12 @@
 import { Game } from './game';
 import { WebSocketClient } from './web-socket/web-socket-client';
 
-window.addEventListener('load', () => {
+window.addEventListener('load', async () => {
     try {
         const context = document.querySelector('canvas').getContext('2d');
-        const webSocketsClient = new WebSocketClient('http://localhost:8081');        
-        const game = new Game(context, webSocketsClient);
+        const webSocketClient = new WebSocketClient('http://localhost:8081');    
+        await webSocketClient.connect();
+        const game = new Game(context, webSocketClient);
         game.run();
     } catch (error) {
         alert(error.message);
