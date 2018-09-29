7f7ad9b708676b7468914280df97edf735725b5f
guidoz <guidoz@LAPTOP-RMMS2JC1>
2018-09-29 12:35:01 -0300

    add message handlers

--- client/src/game.ts
+++ client/src/game.ts
@@ -5,12 +5,29 @@ export class Game {
     constructor(
         private readonly context: CanvasRenderingContext2D,
         private readonly webSocketClient: WebSocketClient
-    ) {}
+    ) {
+        this.registerMessageHandlers();
+    }
 
     run() {
         this.update();
         this.draw();
         requestAnimationFrame(this.run.bind(this));
+    }
+
+    private registerMessageHandlers() {
+        this.webSocketClient.onMessage('requestName', message => {
+            const name = prompt('Enter your name:');
+            this.webSocketClient.emit({
+                event: 'sendName',
+                playerId: 1,
+                payload: { name }
+            });
+        });
+
+        this.webSocketClient.onMessage('disconnect', () => {
+            alert('Disconnected from server');
+        });
     }
 
     private update() {
