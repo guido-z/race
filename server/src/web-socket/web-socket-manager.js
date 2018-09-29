61c3940e86931935c0123f57522a2bd55bbf1841
guidoz <guidoz@LAPTOP-RMMS2JC1>
2018-09-29 13:49:24 -0300

    change web socket client name

--- server/src/web-socket/web-socket-manager.js
+++ server/src/web-socket/web-socket-manager.js
@@ -1,5 +1,5 @@
 module.exports = function (io) {
-    const io = io;
+    const _io = io;
     const clients = new Map();
 
     const addClient = function (id, name) {
@@ -10,14 +10,14 @@ module.exports = function (io) {
         clients.delete(id);
     };
 
-    const emit = function (message) {
-        io.emit(message);
+    const emit = function (message, payload) {
+        _io.emit(message, payload);
     }
 
     const emitToSingleClient = function (message, client) {
         const dest = clients.get(client.conn.id);
         if (dest) {
-            this.io.clients[dest.id].send(message);
+            _io.clients[dest.id].send(message);
         }
     }
 
